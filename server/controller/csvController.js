718409eee9ded1aedb59fa88fe8e9d680ce5bf3a
jash_nikumbhe <jashunik9@gmail.com>
2025-12-30 22:22:12 +0530

    remove accidental function

--- server/controller/csvController.js
+++ server/controller/csvController.js
@@ -39,65 +39,6 @@ const importData = async (req, res) => {
                         const branch = (row.branch || row.Branch).trim();
                         const year = (row.year || row.Year).trim();
 
-                        const importData = async (req, res) => {
-    const filePath = req.file.path;
-    try {
-        if(!req.file)
-            return res.status(400).json({error: "Please upload a CSV File"});
-        const results = [];
-        fs.createReadStream(filePath)
-            .pipe(csv())
-            .on("data", (data) => results.push(data))
-            .on("end", async () => {
-                const importTasks = results.map(async (row) => {
-                    try {
-                        const name = (row.name || row.Name).trim();
-                        const email = (row.email || row.Email).trim();
-                        const phone = (row.phone || row.Phone).trim();
-                        const branch = (row.branch || row.Branch).trim();
-                        const year = (row.year || row.Year).trim();
-
-                        if(!email) 
-                            return { status: 'failed', reason: 'No email found' };
-
-                        const existing = await Participant.findOne({ email });
-                        if(existing) {
-                            console.log(`Skipping ${email} - Already exists.`);
-                            return { status: 'skipped', email };
-                        }
-                        const token = await generateUniqueToken();
-                        const qrData = { name, email, phone, branch, year, token};
-
-                        const qrURL = await generateandUploadQRCode(qrData, `${name}_${token}`);
-                        await Participant.create({
-                            ...qrData,
-                            qrUrl: qrURL,
-                        });
-
-                        const html = registrationEmailTemplate(name, qrURL, token);
-                        mail(email, "Registration Successful", html);
-
-                        return { status: 'success', email };
-                    } catch (err) {
-                        return { status: 'error', email: row.email, message: err.message };
-                    }
-                });
-                const finalUpload = await Promise.allSettled(importTasks);
-                if(fs.existsSync(filePath)) {
-                    fs.unlinkSync(filePath)
-                }
-                const added = finalUpload.filter(r => r.value?.status === 'success').length;
-                const skipped = finalUpload.filter(r => r.value?.status === 'skipped').length;
-                console.log(`Import finished. Added: ${added}, Skipped: ${skipped}`)
-                res.status(200).json({ message: `Import finished. Added: ${added}, Skipped: ${skipped}` });
-            });
-    } catch (error) {
-        if(fs.existsSync(filePath)) 
-            fs.unlinkSync(filePath)
-        console.error("Error importing CSV:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-};
                         const existing = await Participant.findOne({ email });
                         if(existing) {
                             console.log(`Skipping ${email} - Already exists.`);
